2176a3424d6f568090a910956cfb99e472efa1f5
Ashenafi Gichamo <ashenafigichamo@gmail.com>
2018-02-24 20:05:26 -0500

    Added an event listener to every single cell

--- designs.js
+++ designs.js
@@ -1,24 +1,33 @@
 
-const hight = document.getElementById('inputHeight').value;
-const width = document.getElementById('inputWeight').value;
+function makeGrid() {
+    
+const myTable = document.getElementById('pixelCanvas');
+myTable.innerHTML = "";
+// Select size input
+let hight = document.getElementById('inputHeight').value;
+let width = document.getElementById('inputWeight').value;
 console.log(hight);
 console.log(width);
-
-function makeGrid() {
-const myTable = document.getElementById('pixelCanvas');
-for( r=1; r<=hight; r++){
+for(let r=1; r<=hight; r++){
     const trs = document.createElement('tr');
     const updateTable = myTable.appendChild(trs);
     const trss = myTable.appendChild(trs);
     console.log(trss);
-    for( c=1; c<=width; c++){
+    for(let c=1; c<=width; c++){
         const newElement = document.createElement('td');
         updateTable.appendChild(newElement);
         newElement.addEventListener('click', function(){
-        newElement.style.backgroundColor = (document.getElementById('colorPicker').value);
+        // Select color input
+        newElement.style.backgroundColor = document.getElementById('colorPicker').value;
         console.log("was clickeddd");
         });
     }
  }}
-makeGrid();
+
+// When size is submitted by the user, call makeGrid()
+
+ document.getElementById("sizePicker").addEventListener("submit", function(evt) {
+  evt.preventDefault();
+  makeGrid();
+});
 
